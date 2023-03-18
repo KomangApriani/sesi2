9112f00fe69930e1b0de930fa35cf3f7ef26dc4a
KomangApriani <127809809+KomangApriani@users.noreply.github.com>
2023-03-18 12:17:42 +0800

    upload sesi 2

---
+++ sesi2/latihan01.php
@@ -0,0 +1,9 @@
+<?php
+    echo "Belajar Script Php";
+    $dta = "Apriani";
+    $angka = 5; 
+    echo '<br>Nama '.$dta. "<br>Peringkat ke-".$angka;
+    echo '<br>Nama $dta'. "<br>anak ke-".$angka; 
+    echo "<br>Nama : $dta <br>anak ke-$angka";
+    
+
---
+++ sesi2/latihan03.php
@@ -0,0 +1,8 @@
+<?php 
+    $kondisiAwal =1; 
+
+    if($kondisiAwal ==2){
+        echo"Kondisi Awal sama dengan 1";
+    }else{
+        echo"Kondisi Awal tidak sama dengan 1";
+    }
